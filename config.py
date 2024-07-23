f084b33a1db7d12142102e10decaf4137eae2ec4
bommugurl <Sowndharyasowndhrya421@gmail.com>
2024-07-23 08:25:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,13 +22,13 @@ SONG_DOWNLOAD_DURATION = int(
 
 LOGGER_ID = int(getenv("LOGGER_ID", ""))
 
-OWNER_ID = int(getenv("OWNER_ID", "1556830659"))
+OWNER_ID = int(getenv("OWNER_ID", "6616796006"))
 
-START_STICKER_ID = getenv("START_STICKER_ID", "CAACAgUAAxkBAAEMgWhml69Wx7YkDGlt0hkiym2KnrT49QACEAUAAooH4VWxEZDXfVTWqjUE")
+START_STICKER_ID = getenv("START_STICKER_ID", "CAACAgUAAxkBAAEMh05mnxmdTdD_d3MX1bvpZMBduBCQqgACxQ8AAuPNQVVKfyLFzl_KDDUE")
 
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "sarah x music")
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "sowmi ^ Music")
 
-POWERED_BY = getenv("POWERED_BY", "SARAH x MUSIC")
+POWERED_BY = getenv("POWERED_BY", "‹‹‹[🢓 𝕌𝐧𝐢𝐯𝐞𝐫𝐬𝐞 𝐍𝐞𝐭-𝐖𝐨𝐫𝐤𝐬 🢓]›››")
 
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
@@ -36,7 +36,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/SARAHXQuin/SarahxMusic",
+    "https://github.com/bommugurl/sowbowmus",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Main")
 GIT_TOKEN = getenv(
@@ -44,7 +44,7 @@ GIT_TOKEN = getenv(
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/we_are_universee")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/universe_we_are")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Sollatha_vaarthaigal_143")
 
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", None))
 AUTO_SUGGESTION_MODE = getenv("AUTO_SUGGESTION_MODE", "True")
@@ -86,18 +86,18 @@ clean = {}
 autoclean = []
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://te.legra.ph/file/87c57ca975e1c1a71deaf.jpg"
+    "START_IMG_URL", "https://telegra.ph/file/3bbc3c4e06d1476c53a14.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://te.legra.ph/file/86a7ff0f5d480f255fafb.jpg"
+    "PING_IMG_URL", "https://telegra.ph/file/0f19d2acc72fc764446bb.jpg"
 )
-PLAYLIST_IMG_URL = "https://telegra.ph/file/7db32a17b15440fa88f91.jpg"
-STATS_IMG_URL = "https://telegra.ph/file/8f4293af18fdb472c7cfe.jpg"
-TELEGRAM_AUDIO_URL = "https://telegra.ph/file/fa144f06d6d024877edda.jpg"
+PLAYLIST_IMG_URL = "https://telegra.ph/file/af86ccd36db0028a47597.jpg"
+STATS_IMG_URL = "https://telegra.ph/file/80a9ed23ef383cab9120c.jpg"
+TELEGRAM_AUDIO_URL = "https://telegra.ph/file/bad08d50dd445dd12162d.jpg"
 TELEGRAM_VIDEO_URL = "https://telegra.ph/file/fa144f06d6d024877edda.jpg"
-STREAM_IMG_URL = "https://telegra.ph/file/03efec694e41e891b29dc.jpg"
+STREAM_IMG_URL = "https://telegra.ph/file/a695a0c52b7c19c4cbf8c.jpg"
 SOUNCLOUD_IMG_URL = "https://telegra.ph/file/b00af14042edff8b71f2e.jpg"
-YOUTUBE_IMG_URL = "https://telegra.ph/file/9f3720a8d4ec5ae50977f.jpg"
+YOUTUBE_IMG_URL = "https://telegra.ph/file/e8819c35a316479cc3d01.jpg"
 SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph/file/fa144f06d6d024877edda.jpg"
 SPOTIFY_ALBUM_IMG_URL = "https://telegra.ph/file/fa144f06d6d024877edda.jpg"
 SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph/file/fa144f06d6d024877edda.jpg"
