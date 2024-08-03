34ae31c55aee564acbe5085238dc4e44887a8295
timepassmarkus04 <markusmarwin2002@gmail.com>
2024-08-03 15:10:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,9 +6,9 @@ from pyrogram import filters
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID", ""))
+API_ID = int(getenv("API_ID", "29350132"))
 
-API_HASH = getenv("API_HASH", "")
+API_HASH = getenv("API_HASH", "e854995be05edb5bf21f5b84bdc0212f")
 
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 
@@ -26,7 +26,7 @@ OWNER_ID = int(getenv("OWNER_ID", "6616796006"))
 
 START_STICKER_ID = getenv("START_STICKER_ID", "CAACAgUAAxkBAAEMh05mnxmdTdD_d3MX1bvpZMBduBCQqgACxQ8AAuPNQVVKfyLFzl_KDDUE")
 
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "sowmi ^ Music")
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "Amrita ^ MSN")
 
 POWERED_BY = getenv("POWERED_BY", "‹‹‹[🢓 𝕌𝐧𝐢𝐯𝐞𝐫𝐬𝐞 𝐍𝐞𝐭-𝐖𝐨𝐫𝐤𝐬 🢓]›››")
 
@@ -36,15 +36,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/bommugurl/sowbowmus",
+    "https://github.com/timepassmarkus04/Devil2.0",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/we_are_universee")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Sollatha_vaarthaigal_143")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/MNS_botss")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/MNS_botss")
 
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", None))
 AUTO_SUGGESTION_MODE = getenv("AUTO_SUGGESTION_MODE", "True")
@@ -66,7 +66,7 @@ TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 1073741824))
 
 
 # Get your pyrogram v2 session from @Stringene_bot on Telegram
-STRING1 = getenv("STRING_SESSION", "")
+STRING1 = getenv("STRING_SESSION", "BQG_2PQAe8wWePj7LzevRxd7DF-9KOyv8eY7mrRkinJ6g0aHm5Y8fdKvMm0Gaesu16WbplF-Lv_ec2ujmQ4PvCF0MopZp8-0pOIVa4ycFPNmpqrTSGNMoJz3Wnh1F4i_BhvVesd0k4sklF3TEx2EoNKHQ0l8jsDtwljJOxGNW3pfwqEm5zdQO0L7m6UndzTKS0hhUe7MYrrPFaT7AoZy-9uoYis5HfheyREjca4Fmv3_EaQ0rUUhTD7JFj0dgmBFUoifHJG7wadVP4AsyR1tvfd3dONHwauCGkO_hg9bpbSqxqfDsfDdWBCnDjPahcRQ-uAi_zLrvUeDxHMYtxmI2BXnkdnc3AAAAAG85SnWAA")
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
@@ -86,10 +86,10 @@ clean = {}
 autoclean = []
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/3bbc3c4e06d1476c53a14.jpg"
+    "START_IMG_URL", "https://graph.org/file/c3847da256457f7cb057d.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://telegra.ph/file/0f19d2acc72fc764446bb.jpg"
+    "PING_IMG_URL", "https://graph.org/file/c3847da256457f7cb057d.jpg"
 )
 PLAYLIST_IMG_URL = "https://telegra.ph/file/af86ccd36db0028a47597.jpg"
 STATS_IMG_URL = "https://telegra.ph/file/80a9ed23ef383cab9120c.jpg"
